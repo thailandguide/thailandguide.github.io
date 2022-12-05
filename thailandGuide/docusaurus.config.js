86fc22ac64ee69352bf758f2b82f15ff59182efe
DuckenClaw <ducken.claw@gmail.com>
2022-12-05 17:31:45 +0700

    initial commit

--- thailandGuide/docusaurus.config.js
+++ thailandGuide/docusaurus.config.js
@@ -69,7 +69,11 @@ const config = {
             position: 'left',
             label: 'Cities',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            to: '/blog', 
+            label: 'Blog', 
+            position: 'left',
+          },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
