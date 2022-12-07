1837ff1f6446898b3588e6bb36ef561b5a24ed8a
DuckenClaw <ducken.claw@gmail.com>
2022-12-07 17:59:15 +0700

    gitignore

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/duckenclaw/Documents/work/thailandguide.github.io/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/duckenclaw/Documents/work/thailandguide.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/duckenclaw/Documents/work/thailandguide.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/duckenclaw/Documents/work/thailandguide.github.io/src/css/custom.css'),
+];
