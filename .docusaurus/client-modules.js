4a543a2c91c8cf4652b8f1b81e7ccc7efe31c26d
user <user@192.168.0.104>
2022-12-07 22:13:05 +0700

    update .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/maximpetrenko/forty_two/projects/thailandguide.github.io/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/maximpetrenko/forty_two/projects/thailandguide.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/maximpetrenko/forty_two/projects/thailandguide.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/maximpetrenko/forty_two/projects/thailandguide.github.io/src/css/custom.css'),
-];
--- build/assets/js/d15b7cab.f1c0a385.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkthailand_guide=self.webpackChunkthailand_guide||[]).push([[768],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
