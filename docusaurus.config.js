87379b15583d813e5e1f3b700b92b895c4d657cb
Boris Adimov <boris@adimov.ru>
2023-01-09 19:14:25 +0700

    fix description

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/thailandguide/thailandguide.github.io/tree/main/docs',
+            'https://github.com/thailandguide/thailandguide.github.io/tree/main',
         },
         blog: {
           showReadingTime: true,
